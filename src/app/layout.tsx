3f094f28c8fc7f77a5526b9332c98d4f1276293b
Salsa Oktavia Putri <salsaoktavia496@gmail.com>
2025-11-01 18:45:12 +0800

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter, JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+
+const fontSans = Inter({
+  variable: "--font-sans",
+  subsets: ["latin"],
+});
+
+const fontMono = JetBrains_Mono({
+  variable: "--font-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Personal Website-Salsa Oktavia Putri",
+  description: "Salsa Oktavia Putri",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${fontSans.variable} ${fontMono.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
